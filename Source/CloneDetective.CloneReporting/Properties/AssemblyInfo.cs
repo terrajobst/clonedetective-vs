3930f6c7a3cfc7ba739f200e6d4cea1b5001b866
Immo Landwerth <terrajobst@web.de>
2008-07-11 17:10:25 +0000

    Added common assembly info. Added project with MSBuild tasks. Renamed projects so that they all have the common prefix "CloneDetective."

---
+++ Source/CloneDetective.CloneReporting/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: AssemblyTitle("Clone Detective Reporting")]
+[assembly: AssemblyDescription("Contains classes to read and write clone reports")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
