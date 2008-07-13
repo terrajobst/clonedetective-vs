74716036ec0a317f23f7192d72a88fd284c5ac2d
Immo Landwerth <terrajobst@web.de>
2008-07-13 00:30:23 +0000

    Fixed minor incorrectnes in AssemblyInfo.cs of CloneDetective.CloneReporting.

--- Source/CloneDetective.CloneReporting/Properties/AssemblyInfo.cs
+++ Source/CloneDetective.CloneReporting/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Reflection;
 
 [assembly: AssemblyTitle("Clone Detective Reporting")]
-[assembly: AssemblyDescription("Contains classes to read and write clone reports")]
+[assembly: AssemblyDescription("Contains types to read and write clone reports")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
